f393f9b507d4aa9f01f07947f8e227ca676c4ea5
Nielotz <mati2806@poczta.fm>
2020-12-05 23:19:25 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,17 @@
+#pragma once
+
 #include <iostream>
 #include <vector>
-#include <time.h>
+
+#include "profile.cpp"
+#include "game.cpp"
 
 using namespace std;
 
 int main()
 {
+    Profile profile("main.profile");
+    Game game(profile);
+    game.start();
 
-
-	
 }
