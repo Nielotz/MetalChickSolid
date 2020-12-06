11c012753d456b8e4b4a5386f6fbec44c43c6c5b
Nielotz <mati2806@poczta.fm>
2020-12-06 22:09:51 +0100

    Fix data types

--- entity.cpp
+++ entity.cpp
@@ -2,10 +2,12 @@
 
 struct Entity
 {
+    int64_t lvl = 1;
+    int64_t exp = 0;
     int64_t hp = 50;
     int64_t hit = 10;
     uint16_t fight_distance = 0;
-    uint16_t block_chance = 1;
-    uint16_t dodge_chance = 1;
-    uint16_t critic_hit_chance = 1;
+    double block_chance = 1;
+    double dodge_chance = 1;
+    double critic_hit_chance = 1;
 };
