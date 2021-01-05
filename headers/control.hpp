dda3c13b1db038af53c53f189f05a41a39dc634a
Nielotz <mati2806@poczta.fm>
2021-01-05 23:29:38 +0100

    Add WASD controls

--- headers/control.hpp
+++ headers/control.hpp
@@ -1,8 +1,15 @@
 #pragma once
+#include "graphic.hpp"
 
 // Handles input events.
 class Control
 {
 public:
+    // React to game control events.
+    //
+    // Supported events:
+    // Close
+    void parse_events(Graphic& graphic);
     Control() = default;
+
 };
