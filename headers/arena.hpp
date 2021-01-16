8a09d19b84f5c4354b69f5b1686767fd0bdf8c14
Nielotz <mati2806@poczta.fm>
2021-01-16 22:31:09 +0100

    Reformat file

--- headers/arena.hpp
+++ headers/arena.hpp
@@ -10,15 +10,15 @@ enum class FightResult { WIN, DEFEAT, DRAW };
 class Arena
 {
 public:
-    void fight(Hero& hero, Enemy& enemy);
+	void fight(Hero& hero, Enemy& enemy);
 
-    // Returns true, when made a move, false when someone died.
-    //bool do_move(Entity& active_entity, Entity& passive_entity);
+	// Returns true, when made a move, false when someone died.
+	//bool do_move(Entity& active_entity, Entity& passive_entity);
 
-    //FightResult fight(Hero& hero, Entity& enemy);
+	//FightResult fight(Hero& hero, Entity& enemy);
 
 private:
-    void attack(Hero& hero, Enemy& enemy, int32_t who_attack);
-    void hero_defence(Hero& hero, int32_t stamina);
-    int32_t losu();
+	void attack(Hero& hero, Enemy& enemy, int32_t who_attack);
+	void hero_defence(Hero& hero, int32_t stamina);
+	int32_t losu();
 };
